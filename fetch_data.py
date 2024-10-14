7c2856dcd6158d2376b5eeff71b9874284dd717c
TrungKhanhNguyen <khanhnt92@gmail.com>
2024-10-14 23:36:51 +0700

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,12 +1,17 @@
 import requests
+import os
+
+
+app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
+data_base_path = os.path.join(app_base_path, "inference-data")
 
 def download_data(token):
     url = f'https://api.binance.com/api/v3/ticker/price?symbol={token}USDT'
     response = requests.get(url)
-
     if response.status_code == 200:
         data = response.json()
-        file_name = f'inference-data/{token}.txt'
+        os.makedirs(data_base_path, exist_ok=True)
+        file_name = os.path.join(data_base_path, f'{token}.txt')
         with open(file_name, 'w') as file:
             file.write(data['price'])
         print(f'Data for {token} saved to {file_name}')
